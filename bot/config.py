3fc32a9362a9f1cde4c5529c1cd96fe293490650
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2023-04-16 13:46:16 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,12 +18,12 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", default=15681388, cast=int)
     API_HASH = config("API_HASH", default="446b56944f74f6b7688175d48cdfa881")
-    BOT_TOKEN = "5419649199:AAHW1RaJ6VsOMDeiZlMDopg6-CL7eXpi-20"
+    BOT_TOKEN = "6216732508:AAGp2unWzGSS1I0FrSVYeAlxAVehTbfES3o"
     DEV = 5074446156
     OWNER = config("OWNER", default="5074446156")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -map 0:v? -map 0:a? -map 0:s? -c:v libx265 -pix_fmt yuv420p10le -preset medium -vf scale=1920:1080,"drawtext=fontfile=/content/drive/MyDrive/Font/A.ttf:text=Encoded By @Ani_Animesh:x=1680:y=980:fontsize=11:fontcolor=white:enable=between(t\,200\,440)" -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -x265-params frame-threads=4:bframes=8:psy-rd=1.20:aq-mode=3:aq-strength=0.9:deblock=0,-1:vbv-maxrate=30000:vbv-bufsize=30000:vbv-init=0.9 -crf 22 -c:a aac -b:a 128k -c:s copy -metadata title="Presented By Ani Animesh | Anime Sakura" -metadata author="AnimeSakura.co" -metadata copyright="Copyright 2023 Me" -metadata encoded_by="Lolikiller #Sakura_Gang" -metadata:s:0 title="Episode Unknown xD" -metadata:s:a:0 title="Ani Animesh" -metadata:s:a:1 title="AnimeSakura.co" -metadata:s:s:0 title="AnimeSakura.co" -metadata:s:s:1 title="@Ani_Animesh" "{}"',
+        default='ffmpeg -i "{}" -map 0:v? -map 0:a? -map 0:s? -c:v libx265 -pix_fmt yuv420p10le -preset medium -vf scale=1280:720,"drawtext=fontfile=/content/drive/MyDrive/Font/A.ttf:text=Encoded By @Ani_Animesh:x=1680:y=980:fontsize=11:fontcolor=white:enable=between(t\,200\,440)" -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -x265-params frame-threads=4:bframes=8:psy-rd=1.20:aq-mode=3:aq-strength=0.9:deblock=0,-1:vbv-maxrate=30000:vbv-bufsize=30000:vbv-init=0.9 -crf 22.2 -c:a aac -b:a 112k -c:s copy -metadata title="Presented By Ani Animesh | Anime Sakura" -metadata author="AnimeSakura.co" -metadata copyright="Copyright 2023 Me" -metadata encoded_by="Lolikiller #Sakura_Gang" -metadata:s:0 title="Episode Unknown xD" -metadata:s:a:0 title="Ani Animesh" -metadata:s:a:1 title="AnimeSakura.co" -metadata:s:s:0 title="AnimeSakura.co" -metadata:s:s:1 title="@Ani_Animesh" "{}"',
     )
     THUMB = config(
         "THUMBNAIL", default="https://graph.org/file/a615291ef3f9f361e9b12.jpg"
